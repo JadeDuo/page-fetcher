5a79d2192308d1f237b5bf409028904a559ec507
Jade <jade.duong@telus.com>
2022-11-23 14:59:37 -0800

    linted fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,21 @@
 const fs = require('fs');
-const request = require('request')
+const request = require('request');
 
 const fetcher = (url, path) => {
   request(url, (error, response, body) => {
     //let content = `error: ${error}\nresponse: ${response} response status code: ${response.statusCode}\nbody: ${body}`
     fs.writeFile(path, body, (err) =>{
       if (err) {
-        console.log(err)
+        console.log(err);
       } else {
         fs.stat(path, (err, stats) => {
           if (err) {
-            console.log(err)
+            console.log(err);
           } else {
-          console.log(`Downloaded and saved ${stats.size} bytes to ${path}`)
+            console.log(`Downloaded and saved ${stats.size} bytes to ${path}`);
           }
-        })
+        });
       }
-    })
-  })
+    });
+  });
 };
-
-fetcher("http://www.example.com", "./page-data.html")
