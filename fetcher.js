270db828c2456d85529538bbad4b63b006b2b090
Jade <jade.duong@telus.com>
2022-11-23 21:16:13 -0800

    updating fetcher to take in args from command line

--- fetcher.js
+++ fetcher.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 const request = require('request');
+let args = process.argv.slice(2)
+let url = args[0]
+let path = args[1]
 
-const fetcher = (url, path) => {
-  request(url, (error, response, body) => {
-    //let content = `error: ${error}\nresponse: ${response} response status code: ${response.statusCode}\nbody: ${body}`
-    fs.writeFile(path, body, (err) =>{
-      if (err) {
-        console.log(err);
-      } else {
-        fs.stat(path, (err, stats) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(`Downloaded and saved ${stats.size} bytes to ${path}`);
-          }
-        });
-      }
-    });
+request(url, (error, response, body) => {
+  
+  fs.writeFile(path, body, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      fs.stat(path, (err, stats) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Downloaded and saved ${stats.size} bytes to ${path}`);
+        }
+      });
+    }
   });
-};
+});
+
